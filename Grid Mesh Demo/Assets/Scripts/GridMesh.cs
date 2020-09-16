f77b18ca6aa4f20c2263574667e8c144e7abca07
Oleg Frolov <frolololeg@gmail.com>
2020-09-16 21:55:44 +0300

    Started working with ReadMe

---
+++ Grid Mesh Demo/Assets/Scripts/GridMesh.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshCollider))]
+public class GridMesh : MonoBehaviour
+{
+    [Space]  
+    public int xSegments = 8;
+    public int zSegments = 8;
+    [Space]
+    public float xStep = 1.0f;
+    public float zStep = 1.0f;
+    [Space]
+    public Color color = Color.white;
+    [Space]
+    public bool centered = true;
+
+    private float _xOffset = 0.0f;
+    private float _zOffset = 0.0f;
+
+    private readonly List<Vector3> _verticies = new List<Vector3>();
+    private readonly List<int> _indicies = new List<int>();
+
+    private MeshFilter _meshFilter;
+    private Mesh _mesh;
+    private MeshRenderer _meshRenderer;
+    private MeshCollider _meshCollider;
+
+    private void Awake()
+    {
+        if (centered)
+        {
+            _xOffset = xSegments * xStep / 2;
+            _zOffset = zSegments * zStep / 2;
+        }
+        
+        _mesh = new Mesh();
+        _meshFilter = GetComponent<MeshFilter>();
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _meshCollider = GetComponent<MeshCollider>();
+    }
+
+    private void Start()
+    {
+        GenerateVerts();
+        GenerateIndicies();
+        GenerateMesh();
+        _meshCollider.sharedMesh = GenerateCollisionMesh();
+    }
+
+    private void GenerateVerts()
+    {
+        for (int z = 0; z <= zSegments; z++)
+        {
+            for (int x = 0; x <= xSegments; x++)
+            {
+                _verticies.Add(new Vector3(x * xStep - _xOffset, 0, z * zStep - _zOffset));
+            }
+        }
+    }
+
+    private Mesh GenerateCollisionMesh()
+    {
+
+        var mesh = new Mesh();
+
+        Vector3[] points = 
+        {
+            new Vector3(0f - _xOffset, 0f, 0f - _zOffset),
+            new Vector3(0f - _xOffset, 0f, zSegments * zStep - _zOffset),
+            new Vector3(xSegments * xStep - _xOffset, 0f, 0f - _zOffset),
+            new Vector3(xSegments * xStep - _xOffset, 0f, zSegments * zStep - _zOffset)
+        };
+
+        int[] indicies =
+        {
+            0, 1, 2, 2, 1, 3
+        };
+
+        mesh.Clear();
+        mesh.name = "Collision Mesh";
+        mesh.vertices = points;
+        mesh.triangles = indicies;
+
+        return mesh;
+    }
+
+    private void GenerateIndicies()
+    {
+        for (int vert = 0, z = 0; z < zSegments; z++)
+        {
+            for (int x = 0; x < xSegments; x++)
+            {
+                _indicies.Add(vert + 0);
+                _indicies.Add(vert + 1 + xSegments);
+
+                _indicies.Add(vert + 1 + xSegments);
+                _indicies.Add(vert + 2 + xSegments);
+
+                _indicies.Add(vert + 2 + xSegments);
+                _indicies.Add(vert + 1);
+
+                _indicies.Add(vert + 1);
+                _indicies.Add(vert + 0);
+
+                vert++;
+            }
+            vert++;
+        }
+    }
+    
+    private void GenerateMesh()
+    {
+        _mesh.Clear();
+        _mesh.name = "Grid Mesh";
+        _mesh.vertices = _verticies.ToArray();
+        _mesh.SetIndices(_indicies, MeshTopology.Lines, 0);
+        _meshFilter.mesh = _mesh;
+        _meshRenderer.material = new Material(Shader.Find("Sprites/Default")) {color = color};
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# Grid-Mesh
+# Simple Grid Mesh
 Create a mesh based grid for your cool Unity 3D project.
+
+![Simple Grid Mesh](/gridMesh.gif)
+
+## Three simple steps
+1. [Download it](https://raw.githubusercontent.com/Volorf/Hover3D.js/master/hover3D.js);
+2. Import `hover3D.js` to your Unity 3D project;
+3. Add `Grid Mesh` to a gameobject.
+
+🔥 [YouTube Tutorial](https://olegfrolov.design/hover3D/)
+
+## How to set up it via Inspector
+
+## How to set up it via Code
+You have a bunch of properties you can tweak to control the effect.
+```csharp
+[SerializedField] private GridMesh _gridMesh;
+private void Awake() 
+{
+    _gridMesh = GetComponent<GridMesh>();
+}
+private void Start()
+{
+    _gridMesh.xSegments = 16;
+    _gridMesh.ySegments = 16;
+    _gridMesh.color = Color.white;
+    _gridMesh.xStep = 1.0f;
+    _gridMesh.yStep = 1.0f;
+}
+```
+
+## Links
+[Portfolio](https://olegfrolov.design/) | [Linkedin](https://www.linkedin.com/in/oleg-frolov-6a6a4752/) | [Dribbble](https://dribbble.com/Volorf) | [Twitter](https://www.twitter.com/volorf) 
+
